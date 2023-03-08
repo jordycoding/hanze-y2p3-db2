0194b723eb447771f5f03b162fcdb14f8ebeea3c
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2023-03-08 21:14:06 +0100

    Fucking hoer

--- Week2/create_db.sql
+++ Week2/create_db.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS student
 CREATE TABLE IF NOT EXISTS student_theme
 (
     id            INT NOT NULL AUTO_INCREMENT,
-    student_id    INT NOT NULL,
-    theme_id      INT NOT NULL,
+    student_id    INT,
+    theme_id      INT,
     start_year    INT NOT NULL,
     start_quarter INT NOT NULL,
     PRIMARY KEY (id)
@@ -40,34 +40,43 @@ CREATE TABLE IF NOT EXISTS module
 CREATE TABLE IF NOT EXISTS theme_module
 (
     id        INT NOT NULL AUTO_INCREMENT,
-    theme_id  INT NOT NULL,
-    module_id INT NOT NULL,
+    theme_id  INT,
+    module_id INT,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS test
 (
-    id        INT          NOT NULL AUTO_INCREMENT,
-    module_id INT          NOT NULL,
-    name      VARCHAR(255) NOT NULL,
-    credits   INT          NOT NULL,
+    id         INT          NOT NULL AUTO_INCREMENT,
+    module_id  INT,
+    name       VARCHAR(255) NOT NULL,
+    credits    INT          NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS grades
 (
     id         INT NOT NULL AUTO_INCREMENT,
-    student_id INT NOT NULL,
-    module_id  INT NOT NULL,
+    student_id INT,
+    module_id  INT,
     grade      INT NOT NULL,
     PRIMARY KEY (id)
 );
 
-ALTER TABLE student_theme ADD FOREIGN KEY (student_id) REFERENCES student(id);
-ALTER TABLE student_theme ADD FOREIGN KEY (theme_id) REFERENCES theme(id);
-ALTER TABLE test ADD FOREIGN KEY (module_id) REFERENCES module(id);
-ALTER TABLE grades ADD FOREIGN KEY (student_id) REFERENCES student(id);
-ALTER TABLE grades ADD FOREIGN KEY (module_id) REFERENCES module(id);
-ALTER TABLE theme_module ADD FOREIGN KEY (theme_id) REFERENCES theme(id);
-ALTER TABLE theme_module ADD FOREIGN KEY (module_id) REFERENCES module(id);
+ALTER TABLE student_theme
+    ADD FOREIGN KEY (student_id) REFERENCES student (id);
+ALTER TABLE student_theme
+    ADD FOREIGN KEY (theme_id) REFERENCES theme (id);
+ALTER TABLE test
+    ADD FOREIGN KEY (module_id) REFERENCES module (id);
+ALTER TABLE test
+    ADD FOREIGN KEY (student_id) REFERENCES student (id);
+ALTER TABLE grades
+    ADD FOREIGN KEY (student_id) REFERENCES student (id);
+ALTER TABLE grades
+    ADD FOREIGN KEY (module_id) REFERENCES module (id);
+ALTER TABLE theme_module
+    ADD FOREIGN KEY (theme_id) REFERENCES theme (id);
+ALTER TABLE theme_module
+    ADD FOREIGN KEY (module_id) REFERENCES module (id);
 
--- Week2/seed_db.sql
+++ Week2/seed_db.sql
@@ -1,42 +1,17 @@
-INSERT INTO module (name, credits, year, quarter)
-VALUES
-    ('Databases', 6, 1, 2),
-    ('Programming', 6, 1, 1),
-    ('Data Science', 6, 2, 1),
-    ('Algorithms', 6, 2, 2);
+INSERT INTO student (first_name, last_name, starting_year)
+VALUES ('Bram', 'De Vries', 2020);
 
 INSERT INTO theme (name, credits, year, quarter)
-VALUES
-    ('Datastructuren', 12, 1, 1),
-    ('Artificial intelligence', 12, 2, 2);
+VALUES ('Datastructuren', 15, 1, 1);
 
-INSERT INTO student (first_name, last_name, starting_year)
-VALUES
-    ('Lotte', 'Jansen', 2020),
-    ('Daan', 'van der Meer', 2021);
-
-INSERT INTO student_theme (student_id, theme_id, start_year, start_quarter)
-VALUES
-    (1, 1, 2020, 1),
-    (2, 2, 2021, 2);
+INSERT INTO module (name, credits, year, quarter)
+VALUES ('Databases', 5, 1, 1),
+       ('SQL advanced', 5, 1,1),
+        ('NoSQL databases', 5, 1,1);
 
 INSERT INTO theme_module (theme_id, module_id)
-VALUES
-    (1, 2),
-    (2, 1),
-    (2, 4),
-    (1, 3);
+VALUES (1, 1),
+       (1,2),
+       (1,3);
 
-INSERT INTO test (module_id, name, credits)
-VALUES
-    (1, 'AI Programming', 6),
-    (2, 'Relational databases', 6),
-    (4, 'Algorithms and Datastructures', 6);
-
-INSERT INTO grades (student_id, module_id, grade)
-VALUES
-    (1, 2, 8),
-    (1, 1, 7),
-    (2, 2, 9),
-    (2, 1, 6);
-
+INSERT INTO test (module_id, student_id, name, grade)
